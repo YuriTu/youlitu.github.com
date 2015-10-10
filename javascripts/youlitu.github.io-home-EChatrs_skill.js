11d6fc9d89470954237418e2807924ba6152ed30
youli <tuqiang_ru@163.com>
2015-10-10 16:46:06 +0800

    新主页js
    js文件

---
+++ javascripts/youlitu.github.io-home-EChatrs_skill.js
@@ -0,0 +1,134 @@
+
+// 技能树文字云 来自ECharts
+ 
+
+
+function createRandomItemStyle() {
+    return {
+        normal: {
+            color: 'rgb(' + [
+                Math.round(Math.random() * 160),
+                Math.round(Math.random() * 160),
+                Math.round(Math.random() * 160)
+            ].join(',') + ')'
+        }
+    };
+}
+
+var myChart = echarts.init(document.getElementById('EChatrs_skill'))
+
+var option = {
+    title: {
+        text: 'Yuri\'s skill',
+        link: 'https://youlitu.github.io/'
+    },
+    tooltip: {
+        show: true
+    },
+    series: [{
+        name: 'Google Trends',
+        type: 'wordCloud',
+        size: ['80%', '80%'],
+        textRotation : [0, 45, 90, -45],
+        textPadding: 0,
+        autoSize: {
+            enable: true,
+            minSize: 14
+        },
+        data: [
+            {
+                name: "HTML",
+                value: 10000,
+                itemStyle: {
+                    normal: {
+                        color: 'black'
+                    }
+                }
+            },
+            {
+                name: "CSS",
+                value: 8181,
+                itemStyle: createRandomItemStyle()
+            },
+            {
+                name: "Javascripts",
+                value: 7386,
+                itemStyle: createRandomItemStyle()
+            },
+            {
+                name: "JQuery",
+                value: 7005,
+                itemStyle: createRandomItemStyle()
+            },
+            {
+                name: "AJAX",
+                value: 5467,
+                itemStyle: createRandomItemStyle()
+            },
+            {
+                name: "SASS",
+                value: 5244,
+                itemStyle: createRandomItemStyle()
+            },
+            {
+                name: "PHP",
+                value: 3898,
+                itemStyle: createRandomItemStyle()
+            },
+            {
+                name: "MySQL",
+                value: 3484,
+                itemStyle: createRandomItemStyle()
+            },
+            {
+                name: "Linux",
+                value: 2112,
+                itemStyle: createRandomItemStyle()
+            },
+            {
+                name: "Zepio.js",
+                value: 1965,
+                itemStyle: createRandomItemStyle()
+            },
+            {
+                name: "JQuery Mobile",
+                value: 1847,
+                itemStyle: createRandomItemStyle()
+            },
+            {
+                name: "Git ",
+                value: 882,
+                itemStyle: createRandomItemStyle()
+            },
+            {
+                name:"Photoshop",
+                value: 5555,
+                itemStyle: createRandomItemStyle()
+            },
+            {
+                name: "premiere",
+                value: 5550,
+                itemStyle: createRandomItemStyle()
+            },
+            {
+                name: "office",
+                value: 3462,
+                itemStyle: createRandomItemStyle()
+            },
+            {
+                name: "Subline Text",
+                value: 366,
+                itemStyle: createRandomItemStyle()
+            },
+            {
+                name: "тпр-4",
+                value: 360,
+                itemStyle: createRandomItemStyle()
+            }
+            
+        ]
+    }]
+};
+
+myChart.setOption(option);
+                    
---
+++ javascripts/youlitu.github.io-home.js
@@ -0,0 +1,121 @@
+
+// 全局函数
+
+
+// sidebar hover高亮事件
+$(function  () {
+  // sidebar hover高亮事件
+  $("#homeList li").hover(function() {
+    /* Stuff to do when the mouse enters the element */
+    $(this)
+      
+      .stop(true)
+      .animate({"letterSpacing":"5px"}, 500)
+      .css('backgroundColor', '#29a3cc');
+  }, function() {
+    /* Stuff to do when the mouse leaves the element */
+    $(this)
+      
+      .stop(true)
+      .animate({"letterSpacing":"0px"}, 500)
+      .css('backgroundColor', '#0099CC');
+  });
+
+  // 点击事件
+  // home
+  $("#homeList li").click(function() {
+    /* Act on the event */
+    var show_width = $("#introduce ul li").width();
+    // console.log(show_width);
+    $("#introduce ul li[display!=none]")
+      .stop(true)  
+      .animate({        
+        left : show_width +  "px"},
+        1000, function() {
+        /* stuff to do after animation is complete */
+        $(this).css('display', 'none')
+        .end();
+      });
+    // console.log("#page_" + this.id);
+    $("#page_" + this.id)
+      .fadeIn("fast")
+      // .stop(true) 
+      .animate({left : "0px"}, 1000)
+  });
+
+  // 项目title展示事件  
+
+  // 为各个项目添加样式
+  $("#projectOne")
+    .attr('title', '这是我的第一个网站项目，是第一次使用HTML + CSS + 原生JS搭建项目，涉及了一些基础的js交互操作，以及基础的项目管理的知识，因为没有做文件压缩图片压缩等优化，所以访问速度较慢。 避免了新手常见的滥用class div 等误区。');
+  $("#projectTwo")
+    .attr('title', '个人主页的搭建，使用了JQ以及其他插件、库，丰富了网页内容与样式，使用了ECharts搭建文字云，以及Pete Rojwongsuriya的动态背景效果，熟悉和巩固了 JQ 的各种API,为了日后的维护没有压缩各类css和js文件。');
+  // 项目title展示
+  
+  $("div.projectShow")
+    .mouseover(function(event) {
+      /* Act on the event */
+      // 创建
+      // var projectTipId = this.id + "Tip";
+      var x = -290,y = 20;
+      this.myTitle = this.title;
+      this.title = "";
+      var projectTip = "<div id = 'projectTip' >" + this.myTitle +"</div>";
+      $("#page_project").append(projectTip);
+      $("#projectTip")
+            .css({
+            "top": y+event.pageY + "px",
+            "left": x+event.pageX + "px"
+        }).show("fast");
+      // $("#projectTip").remove();
+  })
+    .mouseout(function(event) {
+        /* Act on the event */
+        this.title = this.myTitle;
+        $("#projectTip").remove();
+    })
+    
+    .mousemove(function(event) {
+        /* Act on the event */
+        var x = -290,y = 20;
+      $("#projectTip")
+            .css({
+            "top": y+event.pageY + "px",
+            "left": x+event.pageX + "px"
+        });
+    });
+
+
+    // contact 页面图标动画
+    $("#page_contact div img").rotate({
+      bind:{
+      mouseover:function(){
+        $(this)
+          .stop(true)
+          .rotate({
+          animateTo :360
+        })
+        $(this)
+          .animate({width: "150px", height: "150px"}, "fast")
+      },
+      mouseout:function(){
+        $(this).stop(true)
+        $(this).rotate({
+          animateTo:0
+        })
+        $(this)
+          .animate({width: "175px", height: "175px"}, "fast")
+      }
+
+    }
+
+
+    });
+   
+
+
+
+ 
+})
+
+
